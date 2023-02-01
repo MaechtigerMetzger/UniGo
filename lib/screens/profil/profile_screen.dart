01ece2647c4f348aab6c6e66b1c0030d6fc67f22
David Wolz <david.wolz@informatik.hs-fulda.de>
2023-02-01 12:38:44 +0100

    ProfileScreen verbessert

--- lib/screens/profil/profile_screen.dart
+++ lib/screens/profil/profile_screen.dart
@@ -31,157 +31,180 @@ class ProfileScreen extends StatelessWidget {
         ],
       ),
       backgroundColor: Color.fromARGB(255, 219, 237, 236),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            color: Colors.white,
-            child: Column(
-              children: [
-                SizedBox(height: 80),
-                _buildUser("Max Mustermann"),
-                SizedBox(height: 20),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: 360,
-            height: 150,
-            decoration: BoxDecoration(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Center(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 10),
-                  _buildCaption("Über Mich"),
-                  SizedBox(height: 10),
-                  Text("Hi ich bin Max, 23 Jahre alt."),
-                  SizedBox(height: 15),
-                  _buildCaption("Präferenzen"),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: _buildPreferences("Raucher"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: _buildPreferences("Musik"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: _buildPreferences("E-Auto"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: _buildPreferences("Tiere"),
-                      )
-                    ],
-                  )
+                  SizedBox(height: 80),
+                  _buildUser("Max Mustermann"),
+                  SizedBox(height: 20),
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 25),
-          Container(
-            width: 360,
-            height: 120,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 10),
-                  _buildCaption("User Bewertung"),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.bathtub_outlined,
-                          size: 50,
-                          color: Color.fromARGB(255, 104, 192, 185),
+            SizedBox(height: 20),
+            Container(
+              width: 360,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: _buildCaption("Über Mich"),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Text("Hi ich bin Max, 23 Jahre alt."),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: _buildCaption("Präferenzen"),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: _buildPreferences("Raucher"),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.bathtub_outlined,
-                          size: 50,
-                          color: Color.fromARGB(255, 104, 192, 185),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: _buildPreferences("Musik"),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.bathtub_outlined,
-                          size: 50,
-                          color: Color.fromARGB(255, 104, 192, 185),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: _buildPreferences("E-Auto"),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.bathtub_outlined,
-                          size: 50,
-                          color: Color.fromARGB(255, 104, 192, 185),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: _buildPreferences("Tiere"),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 25),
-          Container(
-            width: 360,
-            height: 150,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 10),
-                  _buildCaption("Fahrzeuge"),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Text("Mazda CX 5"),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16),
-                        child: Icon(
-                          Icons.directions_car_outlined,
-                          size: 50,
-                          color: Color.fromARGB(255, 104, 192, 185),
+            SizedBox(height: 25),
+            Container(
+              width: 360,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: _buildCaption("User Bewertung"),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.bathtub_outlined,
+                            size: 50,
+                            color: Color.fromARGB(255, 104, 192, 185),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Icon(
-                    Icons.add_circle_outline,
-                    size: 50,
-                    color: Color.fromARGB(255, 104, 192, 185),
-                  ),
-                ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.bathtub_outlined,
+                            size: 50,
+                            color: Color.fromARGB(255, 104, 192, 185),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.bathtub_outlined,
+                            size: 50,
+                            color: Color.fromARGB(255, 104, 192, 185),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.bathtub_outlined,
+                            size: 50,
+                            color: Color.fromARGB(255, 104, 192, 185),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          )
-        ],
+            SizedBox(height: 25),
+            Container(
+              width: 360,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: _buildCaption("Fahrzeuge"),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24),
+                          child: Text("Mazda CX 5"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Icon(
+                            Icons.directions_car_outlined,
+                            size: 50,
+                            color: Color.fromARGB(255, 104, 192, 185),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Icon(
+                        Icons.add_circle_outline,
+                        size: 50,
+                        color: Color.fromARGB(255, 104, 192, 185),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
